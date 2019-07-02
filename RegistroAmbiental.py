a2857365e2ddf64947f2a093b592f0557e4509e6
PedroHasalik <pedrohasa97@gmail.com>
2019-07-02 18:40:21 -0300

    empece el programa de la raspberry

---
+++ RegistroAmbiental.py
@@ -0,0 +1,54 @@
+import Adafruit_DHT
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+import RPi.GPIO as GPIO
+
+class Temperatura:
+
+    def __init__(self, pin=17, sensor=Adafruit_DHT.DHT11):
+        self._sensor = sensor
+        self._data_pin = pin
+
+    def leer_datos(self):
+      humedad, temperatura = Adafruit_DHT.read_retry(self._sensor, self._data_pin)
+      return {'temperatura': temperatura, 'humedad': humedad}
+
+class Microfono:
+
+    def __init__(self, canal=22):
+        self._canal = canal
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._canal, GPIO.IN)
+        GPIO.setwarnings(False)
+        GPIO.add_event_detect(self._canal, GPIO.RISING)
+
+    def detectar(self, funcion):
+        if GPIO.event_detected(self._canal):
+            funcion()
+class Matriz:
+    def __init__(self, numero_matrices=1, orientacion=0, rotacion=0, ancho=8, alto=8):
+        self.font = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
+        self.serial = spi(port=0, device=0, gpio=noop())
+        self.device = max7219(self.serial, width=ancho, height=alto, cascaded=numero_matrices, rotate=rotacion)
+
+    def mostrar_mensaje(self, msg, delay=0.1, font=1):
+        show_message(self.device, msg, fill="white",
+                     font=proportional(self.font[font]),
+                     scroll_delay=delay)
+def acciones():
+    print ("Sonido Detectado!")
+    temp_data = temperatura.leer_datos()
+    temp_formateada = 'Temperatura = {0:0.1f}°C  Humedad = {1:0.1f}%'.format(temp_data['temperatura'], temp_data['humedad'])
+    matriz.mostrar_mensaje(temp_formateada, delay=0.08, font=2)
+
+matriz = Matriz()
+microfono = Microfono()
+temperatura = Temperatura()
+
+while true:
+     time.sleep(0.1)
+     microfono.detectar(acciones)
